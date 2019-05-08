d15ba726f9d752ab07e720798ac2e1cbfbd50a8a
hlasimpk <hlasimpk@liv.ac.uk>
2019-05-08 13:42:38 +0100

    Added in container for translation scores

--- simbad/core/phaser_score.py
+++ simbad/core/phaser_score.py
@@ -23,6 +23,29 @@ class PhaserRotationScore(ScoreBase):
         return string.format(name=self.__class__.__name__, **{k: getattr(self, k) for k in self.__slots__})
 
     def _as_dict(self):
-        """Convert the :obj:`PhaserRotationScore <simbad.score.phaser_score.PhaserRotationScore>`
+        """Convert the :obj:`PhaserRotationScore <simbad.core.phaser_score.PhaserRotationScore>`
+        object to a dictionary"""
+        return {k: getattr(self, k) for k in self.__slots__}
+
+
+class PhaserTranslationScore(ScoreBase):
+    """An phaser translation scoring class"""
+
+    __slots__ = ("pdb_code", "dat_path", "rllg", "rfz", "tllg", "tfz")
+
+    def __init__(self, pdb_code, dat_path, rllg, rfz, tllg, tfz):
+        self.pdb_code = pdb_code
+        self.dat_path = dat_path
+        self.rllg = rllg
+        self.rfz = rfz
+        self.tllg = tllg
+        self.tfz = tfz
+
+    def __repr__(self):
+        string = "{name}(pdb_code={pdb_code} dat_path={dat_path} rllg={rllg} rfz={rfz} tllg={tllg} tfz={tfz})"
+        return string.format(name=self.__class__.__name__, **{k: getattr(self, k) for k in self.__slots__})
+
+    def _as_dict(self):
+        """Convert the :obj:`PhaserTranslationScore <simbad.core.phaser_score.PhaserTranslationScore>`
         object to a dictionary"""
         return {k: getattr(self, k) for k in self.__slots__}
